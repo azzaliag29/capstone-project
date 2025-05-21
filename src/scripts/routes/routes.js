90e7c2f75ef0e81df9d7dff342b5a53d56d24321
Azzalia Gusnadi <azzaliag29@gmail.com>
2025-05-22 05:10:51 +0700

    Membuat struktur dan tampilan dari fitur summarize

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
 import LoginPage from '../pages/auth/login/login-page';
 import RegisterPage from '../pages/auth/register/register-page';
+import SummarizePage from '../pages/summarize/summarize-page';
 
 const routes = {
   '/': new HomePage(),
   '/about': new AboutPage(),
   '/login': new LoginPage(),
   '/register': new RegisterPage(),
+  '/summarize': new SummarizePage(),
 };
 
 export default routes;
