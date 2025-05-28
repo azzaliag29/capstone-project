e59188a9571ea8af721190cdb92067071a765d91
Azzalia Gusnadi <azzaliag29@gmail.com>
2025-05-28 12:59:59 +0700

    Eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,15 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
+    extends: ["js/recommended", "plugin:prettier/recommended"],
     languageOptions: { globals: globals.browser },
+    rules: {
+      "prettier/prettier": 0,
+    },
   },
-  eslintPluginPrettier,
 ]);
